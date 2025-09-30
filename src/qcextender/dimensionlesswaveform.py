Fixed match calculation
Fully fixed match calulcation for the mode in the first index of the strain, now resizes both waves properly before calculating the match of the real part.

src/qcextender/dimensionlesswaveform.py
@@ -6,7 +6,7 @@ from numbers import Number
 from qcextender.metadata import Metadata
 from qcextender.waveform import Waveform
 from qcextender.basewaveform import BaseWaveform
-from scipy.interpolate import InterpolatedUnivariateSpline, make_interp_spline
+from scipy.interpolate import make_interp_spline
 
 
 class DimensionlessWaveform(BaseWaveform):
@@ -46,23 +46,26 @@ class DimensionlessWaveform(BaseWaveform):
         if q < 1:
             q = 1 / q
 
+        dt = np.min(np.diff(sim.h.t))
+        sim = sim.h.interpolate(np.arange(sim.h.t[0], sim.h.t[-1], dt))
+
         meta.update(
             library="SXS",
             simulation_id=sim_id,
             q=q,
             modes=list(modes),
-            delta_t=sim.h.t[1] - sim.h.t[0],
+            delta_t=dt,
             dimensionless=True,
         )
 
         single_mode_strain = []
         for l, m in modes:
             try:
-                single_mode_strain.append(np.array(sim.h[:, sim.h.index(l, m)]))
+                single_mode_strain.append(np.array(sim[:, sim.index(l, m)]))
             except:
                 raise ValueError(f"Mode (l={l}, m={m}) not found in this simulation.")
 
-        time = cls._align(np.array(sim.h[:, sim.h.index(l, m)]), sim.h.t)
+        time = cls._align(np.array(sim[:, sim.index(l, m)]), sim.t)
         multi_mode_strain = np.vstack(single_mode_strain)
         metadata = cls._kwargs_to_metadata(meta)
         return cls(multi_mode_strain, time, metadata)
@@ -140,13 +143,13 @@ class DimensionlessWaveform(BaseWaveform):
             except:
                 cutoff = 0
 
-            interpolatedarg = InterpolatedUnivariateSpline(time[cutoff:], arg[cutoff:])(
+            interpolatedarg = make_interp_spline(time[cutoff:], arg[cutoff:])(
                 time[cutoff:]
             )
             # Probably need something smarter than this, start is too sudden instead of gradual like in models
-            interpolatedphase = InterpolatedUnivariateSpline(
-                time[cutoff:], phase[cutoff:]
-            )(time[cutoff:])
+            interpolatedphase = make_interp_spline(time[cutoff:], phase[cutoff:])(
+                time[cutoff:]
+            )
             single_mode_strains.append(interpolatedarg * np.exp(1j * interpolatedphase))
 
         strain = np.vstack(single_mode_strains)

src/qcextender/waveform.py
@@ -1,12 +1,14 @@
 import numpy as np
 from typing import Iterable, Sequence, Self
-from pycbc.waveform import get_td_waveform
+from scipy.interpolate import make_interp_spline
 from pycbc.psd import aLIGOZeroDetHighPower
 from pycbc.types import timeseries as ts
 from pycbc.filter.matchedfilter import match as cbcmatch
 from qcextender.metadata import Metadata
 from qcextender.basewaveform import BaseWaveform
 from qcextender.utils import lal_waves
+
+# from pycbc.waveform import get_td_waveform
 
 
 class Waveform(BaseWaveform):
@@ -104,18 +106,23 @@ class Waveform(BaseWaveform):
             psd (str, optional): The PSD to use in the match. Defaults to "aLIGOZeroDetHighPower".
 
         Returns:
-            float: The real match of the two waveforms.
+            float: The match of the real part of two modes.
         """
+        delta_t = max(self.metadata.delta_t, waveform.metadata.delta_t)
+        wf1_time = np.arange(self.time[0], self.time[-1], delta_t)
+        wf2_time = np.arange(waveform.time[0], waveform.time[-1], delta_t)
 
-        wf1 = ts.TimeSeries(self[2, 2].real, delta_t=self.metadata.delta_t)
-        wf2 = ts.TimeSeries(waveform[2, 2].real, delta_t=waveform.metadata.delta_t)
+        wf1_strain = make_interp_spline(self.time, self.strain[0])(wf1_time)
+        wf2_strain = make_interp_spline(waveform.time, waveform.strain[0])(wf2_time)
+
+        wf1 = ts.TimeSeries(wf1_strain.real, delta_t=delta_t)
+        wf2 = ts.TimeSeries(wf2_strain.real, delta_t=delta_t)
 
         if f_lower is None:
             f_lower = max(self.metadata.f_lower, waveform.metadata.f_lower)
 
-        print(wf1.delta_t, wf2.delta_t)
         flen = 1 << (max(len(wf1), len(wf2)) - 1).bit_length()
-        delta_f = 1.0 / (flen * wf1.delta_t)
+        delta_f = 1.0 / (flen * delta_t)
 
         psd = aLIGOZeroDetHighPower(flen, delta_f, f_lower)
         wf1.resize(flen)